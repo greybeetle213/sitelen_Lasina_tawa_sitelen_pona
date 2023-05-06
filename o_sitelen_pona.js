3fc524b161f05390c58bad975c5b63341131807d
greybeetle213 <65577606+greybeetle213@users.noreply.github.com>
2023-05-06 12:24:03 +1200

    Add files via upload

---
+++ o_sitelen_pona.js
@@ -0,0 +1,182 @@
+function Start(){
+    document.getElementById("waitWarning").innerHTML = "mi ante e sitelen. o awen lon tenpo lili."
+    setTimeout(MakeSitelenPona, 1)
+}
+function MakeSitelenPona(){
+    var _pj;
+    console.log("started")
+    var allWords, bestFit, containerGlyfs, currentFit, file, fineshedSpWord, nameToSP, nonContainableGlyfs, output, validWord, visualBestFit, weirdGlyfs;
+
+    function _pj_snippets(container) {
+    function in_es6(left, right) {
+        if (right instanceof Array || typeof right === "string") {
+        return right.indexOf(left) > -1;
+        } else {
+        if (right instanceof Map || right instanceof Set || right instanceof WeakMap || right instanceof WeakSet) {
+            return right.has(left);
+        } else {
+            return left in right;
+        }
+        }
+    }
+
+    container["in_es6"] = in_es6;
+    return container;
+    }
+
+    _pj = {};
+
+    _pj_snippets(_pj);
+
+    containerGlyfs = ["anpa", "ijo", "jan", "kala", "kili", "kiwen", "ko", "lawa", "len", "lipu", "luka", "lupa", "mije", "nena", "pi", "pilin", "poka", "poki", "pona", "sitelen", "sona", "suno", "tomo", "toki", "tonsi", "walo", "wile"];
+    nonContainableGlyfs = ["anu", "e", "en", "kama", "kepeken", "la", "li", "lon", "pi", "tan", "tawa", "sama"];
+    weirdGlyfs = ["luka"];
+    allWords = ["a", "akesi", "ala", "alasa", "ale", "ali", "anpa", "ante", "anu", "awen", "en", "esun", "ijo", "ike", "ilo", "insa", "jaki", "jan", "jelo", "jo", "kala", "kalama", "kama", "kasi", "ken", "kepeken", "kili", "kin", "kiwen", "ko", "kon", "kule", "kulupu", "kute", "la", "lape", "laso", "lawa", "len", "lete", "lili", "linja", "lipu", "loje", "lon", "luka", "lukin", "lupa", "ma", "mama", "mani", "meli", "mi", "mije", "moku", "moli", "monsi", "mu", "mun", "musi", "mute", "namako", "nanpa", "nasa", "nasin", "nena", "ni", "nimi", "noka", "o", "oko", "olin", "ona", "open", "pakala", "pali", "palisa", "pan", "pana", "pi", "pilin", "pimeja", "pini", "pipi", "poka", "poki", "pona", "pu", "sama", "seli", "selo", "seme", "sewi", "sijelo", "sike", "sin", "sina", "sinpin", "sitelen", "sona", "soweli", "suli", "suno", "supa", "suwi", "tan", "taso", "tawa", "telo", "tenpo", "toki", "tomo", "tu", "uta", "utala", "walo", "wan", "waso", "wawa", "weka", "wile"];
+    file = document.getElementById("input").value
+
+    function update_str(string, index, new_char) {
+    var a_list;
+    a_list = string.split("");
+    a_list[index] = new_char;
+    return a_list.join("");
+    }
+
+    function splitMora(word) {
+    var splitword;
+    splitword = word.split("");
+
+    for (var letter = 0, _pj_a = splitword.length; letter < _pj_a; letter += 1) {
+        if (letter >= splitword.length - 1) {
+        break;
+        }
+
+        if (_pj.in_es6(splitword[letter], "ptksmnljw") && _pj.in_es6(splitword[letter + 1], "aeiou")) {
+        splitword[letter] = splitword[letter] + splitword.splice(letter + 1,1)[0];
+        }
+    }
+
+    return splitword;
+    }
+
+    for (var i = 0, _pj_a = file.length; i < _pj_a; i += 1) {
+    if (!_pj.in_es6(file[i], "abcdefghijklmnopqrstuvqxwyzABCDEFGHIJKLMNOPQRSTUVWXYZ     ")) {
+        if (file[i - 1] !== " ") {
+        file = update_str(file, i, " " + file[i]);
+        i += 1;
+        }
+
+        if (i !== file.length && file[i + 1] !== " ") {
+        file = update_str(file, i, file[i] + " ");
+        }
+    }
+    }
+
+    file = file.split(" ");
+
+    for (var word = 0, _pj_a = file.length - 1; word < _pj_a; word += 1) {
+    if (word >= file.length - 1) {
+        break;
+    }
+
+    validWord = true;
+
+    if (_pj.in_es6(file[word], containerGlyfs) && !_pj.in_es6(file[word + 1], nonContainableGlyfs)) {
+        for (var letter, _pj_d = 0, _pj_b = file[word + 1], _pj_c = _pj_b.length; _pj_d < _pj_c; _pj_d += 1) {
+        letter = _pj_b[_pj_d];
+
+        if (!_pj.in_es6(letter, "ptksmnljwaeiou")) {
+            validWord = false;
+        }
+        }
+
+        if (!_pj.in_es6(file[word], weirdGlyfs) && validWord) {
+        file[word] = file[word] + "-" + file.splice(word + 1,1)[0];
+        } else {
+        if (validWord) {
+            file[word] = file[word] + " -" + file.splice(word + 1,1)[0];
+        }
+        }
+    }
+    }
+
+    for (var word = 0, _pj_a = file.length; word < _pj_a; word += 1) {
+    if (file[word].toLowerCase() !== file[word]) {
+        nameToSP = splitMora(file[word].toLowerCase());
+        console.log("word" + nameToSP.toString());
+        fineshedSpWord = "";
+
+        while (true) {
+        bestFit = ["", 0];
+
+        for (var dictanaryWord, _pj_d = 0, _pj_b = allWords, _pj_c = _pj_b.length; _pj_d < _pj_c; _pj_d += 1) {
+            dictanaryWord = _pj_b[_pj_d];
+            currentFit = 0;
+
+            for (var Mora = 0, _pj_e = nameToSP.length; Mora < _pj_e; Mora += 1) {
+            if (splitMora(dictanaryWord)[Mora] === nameToSP[Mora]) {
+                currentFit += 1;
+            } else {
+                break;
+            }
+
+            if (currentFit === splitMora(dictanaryWord).length) {
+                break;
+            }
+            }
+
+            if (currentFit === 0) {
+            console.log(nameToSP)
+            if (dictanaryWord[0] === nameToSP[0][0]) {
+                currentFit = 0.5;
+            }
+            }
+
+            if (currentFit > bestFit[1]) {
+            bestFit[1] = currentFit;
+            bestFit[0] = dictanaryWord;
+            }
+        }
+
+        if (bestFit[1] === 0) {
+            nameToSP.splice(0,1)        }
+
+        if (bestFit[1] === 0.5) {
+            fineshedSpWord += "_" + bestFit[0];
+            console.log(nameToSP);
+            nameToSP[0] = update_str(nameToSP[0], 0, "");
+            console.log(nameToSP);
+
+            if (nameToSP[0] === "") {
+                nameToSP.splice(0,1)
+            }
+        }
+
+        if (bestFit[1] > 0.5) {
+            visualBestFit = bestFit[1];
+
+            if (nameToSP[0].length === 1) {
+            visualBestFit -= 1;
+            }
+
+            nameToSP = nameToSP.slice(bestFit[1]);
+
+            if (bestFit[1] !== splitMora(dictanaryWord).length) {
+            fineshedSpWord += "_" + bestFit[0] + "_.".repeat(visualBestFit);
+            } else {
+            fineshedSpWord += "_" + bestFit[0] + "_:";
+            }
+        }
+
+        if (nameToSP.length == 0) {
+            break;
+        }
+        }
+
+        console.log("[" + fineshedSpWord + "]");
+        file[word] = "[" + fineshedSpWord + "]";
+    }
+    }
+    console.log(file.join(" "))
+    document.getElementById("waitWarning").innerHTML = "pini"
+    document.getElementById("output").value = file.join(" ");
+}
